8c0dfef53c61312d0db069b3470d3f53c31fb3eb
MatheusOtenio <146143568+MatheusOtenio@users.noreply.github.com>
2025-01-17 11:02:50 -0300

    Update teste1.js

--- teste1.js
+++ teste1.js
@@ -1,1 +1,25 @@
+// Declarando as variáveis
+let petDog = "Rex";
+let petCat = "Pepper";
 
+// Registrando as variáveis no console
+console.log(petDog);
+console.log(petCat);
+
+// Registrando os textos e variáveis no console
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+
+// Declarando variáveis adicionais para os sons
+let catSound = "purr";
+let dogSound = "woof";
+
+// Registrando os sons no console
+console.log(petDog + " says " + dogSound);
+console.log(petCat + " says " + catSound);
+
+// Reatribuindo o valor de catSound
+catSound = "meow";
+
+// Registrando a mudança no console
+console.log(petCat + " now says " + catSound);
