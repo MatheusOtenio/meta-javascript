a58204ff2f00b9c6d29f81422f2affa5ccc18eed
MatheusOtenio <146143568+MatheusOtenio@users.noreply.github.com>
2025-01-19 09:58:59 -0300

    Update teste5.js

--- teste5.js
+++ teste5.js
@@ -1,1 +1,39 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+
+function logDairy() {
+    for (const item of dairy) {
+        console.log(item);
+    }
+}
+
+logDairy();
+
+
+// Task 2
+const animal = {
+    canJump: true
+};
+
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (const [key, value] of Object.entries(bird)) {
+        console.log(`${key}: ${value}`);
+    }
+}
+
+birdCan();
+
+
+// Task 3
+function animalCan() {
+    for (const key in bird) {
+        console.log(`${key}: ${bird[key]}`);
+    }
+}
+
+animalCan();
 
